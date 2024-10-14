4dc1f4c1c6c064959b553158096a720af4f639d6
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-10-14 23:06:12 +0200

    fix: use connection string from env vars

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,11 +1,7 @@
 import { Client } from 'pg';
 
 const client = new Client({
-    user: 'mahmoudelawadi',
-    password: 'iamvagner86',
-    host: 'localhost',
-    port: 5432,
-    database: 'football_db'
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default client;
