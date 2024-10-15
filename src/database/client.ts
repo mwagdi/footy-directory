4385fe5b4cd8a33c9ba8fc61e163730a970d64bd
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-10-15 23:19:57 +0200

    chore: set ssl setting

--- src/database/client.ts
+++ src/database/client.ts
@@ -2,7 +2,9 @@ import { Client } from 'pg';
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default client;
