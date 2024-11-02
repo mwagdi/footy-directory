86adcf53310b2a5fcfbc0c63ba4d38d99433347c
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-11-02 11:27:49 +0100

    fix: return ssl settings

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,7 +1,12 @@
 import { Client } from 'pg';
 
+console.log(process.env);
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default client;
